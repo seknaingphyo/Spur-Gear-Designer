7e323e080b0a6aefaf23d0455f123d20da32e3d8
seknaingphyo <snpnineeleven@gmail.com>
2020-08-08 18:57:40 +0630

    some changes

--- LewisFactor.m
+++ LewisFactor.m
@@ -4,6 +4,10 @@ table = readtable("Lewis_factor_table.xlsx");
 
 idx = ismember(table.Numofteeth, numofteeth);
 
-y = table2array(table(idx,2));
+if isempty(find(idx > 0, 1)) ~= 1
+    y = table2array(table(idx,2));
+elseif isempty(find(idx > 0, 1)) == 1
+    y = table2array(interp1([table(idx - 1,1) table(idx - 1,2)], [table(idx + 1,1) table(idx - 1, 2)], numofteeth));
+end
 
 end
