74cda272e89459d078b65fcd79dcee7821fca006
seknaingphyo <snpnineeleven@gmail.com>
2020-08-08 22:19:08 +0630

    Interpolation of LewisFactor

--- LewisFactor.m
+++ LewisFactor.m
@@ -7,7 +7,11 @@ idx = ismember(table.Numofteeth, numofteeth);
 if isempty(find(idx > 0, 1)) ~= 1
     y = table2array(table(idx,2));
 elseif isempty(find(idx > 0, 1)) == 1
-    y = interp1([table(idx - 1,1) table(idx - 1,2)], [table(idx + 1,1) table(idx - 1, 2)], numofteeth);
+    numofteeth1 = min(table.Numofteeth(table.Numofteeth > numofteeth));
+    numofteeth2 = max(table.Numofteeth(table.Numofteeth < numofteeth));
+    idx1 = ismember(table.Numofteeth, numofteeth1);
+    idx2 = ismember(table.Numofteeth, numofteeth2);
+    y = interp1([numofteeth1 numofteeth2], [table2array(table(idx1, 2)) table2array(table(idx2, 2))], numofteeth, 'linear', 'extrap');
 end
 
 end
