db1f8022a78dc57797fbff9d2e166bd5388f6cdb
seknaingphyo <snpnineeleven@gmail.com>
2020-08-09 15:50:01 +0630

    Update ModuleSelector.m

--- ModuleSelector.m
+++ ModuleSelector.m
@@ -1,7 +1,7 @@
-function m = ModuleSelector(m)
+function module = ModuleSelector(module)
 
 module_series = [1 1.25 1.5 2 2.5 3 4 5 6 8 10 12 16 20 25 32 40 50];
-[~,ix] = min(abs(module_series - m));
-m = module_series(ix);
+[~,ix] = min(abs(module_series - module));
+module = module_series(ix);
 
 end
