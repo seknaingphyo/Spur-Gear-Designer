e0019d9108c8f4db881f4bc71943cf1ed4f99d81
seknaingphyo <snpnineeleven@gmail.com>
2020-08-09 14:28:31 +0630

    Update S_o.m

--- S_o.m
+++ S_o.m
@@ -2,13 +2,7 @@ function strength = S_o(material)
 
 table = readtable("S_o.xlsx");
 
-if material == 1
-    strength = table.S_o(1,:);
-elseif material == 2
-    strength = table.S_o(2,:);
-elseif material == 3
-    strength = table.S_o(3,:);
-end
+strength = table.S_o(material,:);
 
 
 end
