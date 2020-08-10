ce92c508524d73d050ce099dd8f286ee35ac40e1
seknaingphyo <snpnineeleven@gmail.com>
2020-08-10 23:37:47 +0630

    Update S_o.m

--- S_o.m
+++ S_o.m
@@ -2,7 +2,7 @@ function strength = S_o(material)
 
 table = readtable("S_o.xlsx");
 
-idx = strncmp(material, table.Material, 8);   
+idx = strncmp(material, table.Material, 9);   
 
 strength = table.S_o(idx);
 
