583094b9108136833e8ef785b33308dc2befc853
seknaingphyo <snpnineeleven@gmail.com>
2020-08-11 21:23:49 +0630

    Update StressFatigueFactor.m

--- StressFatigueFactor.m
+++ StressFatigueFactor.m
@@ -1,30 +1,32 @@
-function K = StressFatigueFactor(material, BHN1, BHN2, profile)
+function K = StressFatigueFactor(material1, material2, profile)
 
-if strncmp(material, 'Steel', 5) == 1 && strncmp(profile, 'Involute 14.5', 13) == 1
+Steel1 = strncmp(material1, 'Steel', 5);
+Steel2 = strncmp(material2, 'Steel', 5);
+Bronze1 = strncmp(material1, 'Bronze', 6);
+Bronze2 = strncmp(material2, 'Bronze', 6);
+Cast_Iron1 = strncmp(material1, 'Cast Iron', 9);
+Cast_Iron2 = strncmp(material2, 'Cast Iron', 9);
+profile14 = strncmp(profile, 'Involute 14.5', 13);
+profile20 = strncmp(profile, 'Full Depth 20', 13) | strncmp(profile, 'Stub 20', 7);
+
+if Steel1 == 1 && Steel2 == 1 && profile14 == 1
     table = readtable('Stress Fatigue Factor.xlsx', 'Sheet', 1);
     avg_BHN = (BHN1 + BHN2)/2;
     idx = table.Average_BHN_steel(avg_BHN);
     K = table.K_14(idx);
-elseif (strncmp(material, 'Steel', 5) == 1 && strncmp(profile, 'Involute 20', 13) == 1) || (strncmp(material, 'Steel', 5) == 1 && strncmp(profile, 'Stub 20', 7) == 1)
+elseif Steel1 == 1 && Steel2 == 1 && profile20 == 1
     table = readtable('Stress Fatigue Factor.xlsx', 'Sheet', 1);
     avg_BHN = (BHN1 + BHN2)/2;
     idx = table.Average_BHN_steel(avg_BHN);
     K = table.K_20(idx);
-elseif strncmp(material, 'Cast Iron', 9) == 1 && strncmp(profile, 'Involute 14.5', 13) == 1
+elseif (Cast_Iron1 == 1 && Cast_Iron2 == 1 && profile14 == 1) || (Bronze1 == 1 && Bronze2 == 1 && profile14 == 1)
     table = readtable('Stress Fatigue Factor.xlsx', 'Sheet', 3);
     idx = table.Material(material);
     K = table.K_14(idx);
-elseif (strncmp(material, 'Cast Iron', 9) == 1 && strncmp(profile, 'Involute 20', 13) == 1) || (strncmp(material, 'Cast Iron', 9) == 1 && strncmp(profile, 'Stub 20', 7) == 1)
+elseif (Cast_Iron1 == 1 && Cast_Iron2 == 1 && profile14 == 1) || (Bronze1 == 1 && Bronze2 == 1 && profile20 == 1)
     table = readtable('Stress Fatigue Factor.xlsx', 'Sheet', 3);
     idx = table.Material(material);
     K = table.K20(idx);
-elseif strncmp(material, 'Bronze', 6) == 1 && strncmp(profile, 'Involute 14.5', 13) == 1
-    table = readtable('Stress Fatigue Factor.xlsx', 'sheet', 3);
-    idx = table.Material(material);
-    K = table.K_14(idx);
-elseif (strncmp(material, 'Bronze', 6) == 1 && strncmp(profile, 'Involute 20', 13) == 1) || (strncmp(material, 'Bronze', 9) == 1 && strncmp(profile, 'Stub 20', 7) == 1)
-    table = readtable('Stress Fatigue Factor.xlsx', 'sheet', 3);
-    idx = table.Material(material);
-    K = table.K_20(idx);
 end
+
 end
