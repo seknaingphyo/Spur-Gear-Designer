23f16a7724092f5d96967f00dff5baa2275dc4cf
seknaingphyo <snpnineeleven@gmail.com>
2020-08-12 12:37:23 +0630

    Update StressFatigueFactor.m

--- StressFatigueFactor.m
+++ StressFatigueFactor.m
@@ -1,4 +1,4 @@
-function K = StressFatigueFactor(material1, material2, profile)
+function K = StressFatigueFactor(material1, material2, BHN, profile)
 
 Steel1 = strncmp(material1, 'Steel', 5);
 Steel2 = strncmp(material2, 'Steel', 5);
@@ -11,22 +11,32 @@ profile20 = strncmp(profile, 'Full Depth 20', 13) | strncmp(profile, 'Stub 20', 
 
 if Steel1 == 1 && Steel2 == 1 && profile14 == 1
     table = readtable('Stress Fatigue Factor.xlsx', 'Sheet', 1);
-    avg_BHN = (BHN1 + BHN2)/2;
-    idx = table.Average_BHN_steel(avg_BHN);
+    idx = table.Average_BHN_steel(BHN);
     K = table.K_14(idx);
 elseif Steel1 == 1 && Steel2 == 1 && profile20 == 1
     table = readtable('Stress Fatigue Factor.xlsx', 'Sheet', 1);
-    avg_BHN = (BHN1 + BHN2)/2;
-    idx = table.Average_BHN_steel(avg_BHN);
+    idx = table.Average_BHN_steel(BHN);
     K = table.K_20(idx);
 elseif (Cast_Iron1 == 1 && Cast_Iron2 == 1 && profile14 == 1) || (Bronze1 == 1 && Bronze2 == 1 && profile14 == 1)
     table = readtable('Stress Fatigue Factor.xlsx', 'Sheet', 3);
-    idx = table.Material(material);
+    idx = table.Material(material1) | table.Material(material2);
     K = table.K_14(idx);
-elseif (Cast_Iron1 == 1 && Cast_Iron2 == 1 && profile14 == 1) || (Bronze1 == 1 && Bronze2 == 1 && profile20 == 1)
+elseif (Cast_Iron1 == 1 && Cast_Iron2 == 1 && profile20 == 1) || (Bronze1 == 1 && Bronze2 == 1 && profile20 == 1)
     table = readtable('Stress Fatigue Factor.xlsx', 'Sheet', 3);
-    idx = table.Material(material);
-    K = table.K20(idx);
+    idx = table.Material(material1) | table.Material(material2);
+    K = table.K_20(idx);
+elseif Steel1 == 1 && Cast_Iron2 == 1 && profile14 == 1
+    table = readtable('Stress Fatigue Factor.xlsx', 'Sheet', 2);
+    K = table.K_14((table.BHN_steel == 150 & string(table.other_material) == "Cast Iron"), :);
+elseif Steel1 == 1 && Cast_Iron2 == 1 && profile20 == 1
+    table = readtable('Stress Fatigue Factor.xlsx', 'Sheet', 2);
+    K = table.K_20((table.BHN_steel == 150 & string(table.other_material) == "Cast Iron"), :);
+elseif Steel1 == 1 && Bronze2 == 1 && profile14 == 1
+    table = readtable('Stress Fatigue Factor.xlsx', 'Sheet', 2);
+    K = table.K_14((table.BHN_steel == 150 & string(table.other_material) == "Bronze"), :);
+elseif Steel1 == 1 && Bronze2 == 1 && profile20 == 1
+    table = readtable('Stress Fatigue Factor.xlsx', 'Sheet', 2);
+    K = table.K_20((table.BHN_steel == 150 & string(table.other_material) == "Bronze"), :);
 end
 
 end
