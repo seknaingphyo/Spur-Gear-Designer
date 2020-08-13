df184d37aa62bf81a3ea4384a31e7f439041ff25
seknaingphyo <snpnineeleven@gmail.com>
2020-08-13 19:13:12 +0630

    Create tootherror.m

---
+++ tootherror.m
@@ -0,0 +1,21 @@
+function e = tootherror(speed)
+
+table = readtable('Tootherror.xlsx');
+
+idx = ismember(table.Speed, speed);
+
+if isempty(find(idx > 0, 1)) ~= 1
+    e = table2array(table(idx,2));
+elseif isempty(find(idx > 0, 1)) == 1
+    speed1 = min(table.Speed(table.Speed > speed));
+    speed2 = max(table.Speed(table.Speed < speed));
+    idx1 = ismember(table.Speed, speed1);
+    idx2 = ismember(table.Speed, speed2);
+    e = interp1([speed1 speed2],...
+        [table2array(table(idx1, 2)) table2array(table(idx2, 2))],...
+        speed, 'linear', 'extrap');
+end
+
+
+
+end
