8e41f6e869a8ea648fc7da952dbfdc11b3224fb8
seknaingphyo <snpnineeleven@gmail.com>
2020-08-18 15:53:55 +0630

    Update tootherror.m

--- tootherror.m
+++ tootherror.m
@@ -1,35 +1,32 @@
-function e = tootherror(true_module, speed)
+function error = tootherror(true_module, speed)
 
-e_max = maxtootherror(speed);
+error_max = maxtootherror(speed);
 data = readtable('tootherrorvsmodule.xlsx');
-equal = ismember(data.Module, true_module);
-
-if isempty(find(equal > 0, 1)) ~= 1
-    e = max(e_max > table2array(data(equal, 2:4)));
-elseif isempty(find(equal > 0, 1)) == 1
-    m1 = min(data.Module(data.Module > true_module));
-    m2 = max(data.Module(data.Module < true_module));
-    equal1 = ismember(data.Module, m1);
-    equal2 = ismember(data.Module, m2);
-    e1 = table2array(data(equal1, max(data(e_max > data(equal, 2:4)))));
-    e2 = table2array(data(equal2, max(data(e_max > data(equal, 2:4)))));
-    e = interp1([e1 e2], true_module);
+errors = data{data.Module == true_module, 2:end};
+if isempty(errors)
+    error =...
+        [interp1(data.Module,data.Firstclass, true_module)...
+        interp1(data.Module,data.Carefullycut, true_module)...
+        interp1(data.Module,data.Precision, true_module)];
+end
+error = error(error < error_max);
+error = max(error);
 end
     
 
-function e_max = maxtootherror(speed)
+function error_max = maxtootherror(speed)
 
 table = readtable('maxtootherror.xlsx');
 idx = ismember(table.Speed, speed);
 
 if isempty(find(idx > 0, 1)) ~= 1
-    e_max = table2array(table(idx,2));
+    error_max = table2array(table(idx,2));
 elseif isempty(find(idx > 0, 1)) == 1
     speed1 = min(table.Speed(table.Speed > speed));
     speed2 = max(table.Speed(table.Speed < speed));
     idx1 = ismember(table.Speed, speed1);
     idx2 = ismember(table.Speed, speed2);
-    e_max = interp1([speed1 speed2],...
+    error_max = interp1([speed1 speed2],...
         [table2array(table(idx1, 2)) table2array(table(idx2, 2))],...
         speed, 'linear');
 end
