588a82002939ccf61717a82ac0a6d7213961de57
seknaingphyo <snpnineeleven@gmail.com>
2020-08-20 11:38:16 +0630

    Update StressFatigueFactor.m

--- StressFatigueFactor.m
+++ StressFatigueFactor.m
@@ -2,8 +2,6 @@ function K = StressFatigueFactor(material1, material2, BHN, profile)
 
 Steel1 = strncmp(material1, 'Steel', 5);
 Steel2 = strncmp(material2, 'Steel', 5);
-Bronze1 = strncmp(material1, 'Bronze', 6);
-Bronze2 = strncmp(material2, 'Bronze', 6);
 Cast_Iron1 = strncmp(material1, 'Cast Iron', 9);
 Cast_Iron2 = strncmp(material2, 'Cast Iron', 9);
 profile14 = strncmp(profile, 'Involute 14.5', 13);
@@ -20,11 +18,11 @@ elseif Steel1 == 1 && Steel2 == 1 && profile20 == 1
     table = readtable(filename, 'Sheet', 1);
     idx = table.Average_BHN_steel(BHN);
     K = table.K_20(idx);
-elseif (Cast_Iron1 == 1 && Cast_Iron2 == 1 && profile14 == 1) || (Bronze1 == 1 && Bronze2 == 1 && profile14 == 1)
+elseif Cast_Iron1 == 1 && Cast_Iron2 == 1 && profile14 == 1
     table = readtable(filename, 'Sheet', 3);
     idx = table.Material(material1) | table.Material(material2);
     K = table.K_14(idx);
-elseif (Cast_Iron1 == 1 && Cast_Iron2 == 1 && profile20 == 1) || (Bronze1 == 1 && Bronze2 == 1 && profile20 == 1)
+elseif Cast_Iron1 == 1 && Cast_Iron2 == 1 && profile20 == 1
     table = readtable(filename, 'Sheet', 3);
     idx = table.Material(material1) | table.Material(material2);
     K = table.K_20(idx);
@@ -34,12 +32,6 @@ elseif Steel1 == 1 && Cast_Iron2 == 1 && profile14 == 1
 elseif Steel1 == 1 && Cast_Iron2 == 1 && profile20 == 1
     table = readtable(filename, 'Sheet', 2);
     K = table.K_20((table.BHN_steel == 150 & string(table.other_material) == "Cast Iron"), :);
-elseif Steel1 == 1 && Bronze2 == 1 && profile14 == 1
-    table = readtable(filename, 'Sheet', 2);
-    K = table.K_14((table.BHN_steel == 150 & string(table.other_material) == "Bronze"), :);
-elseif Steel1 == 1 && Bronze2 == 1 && profile20 == 1
-    table = readtable(filename, 'Sheet', 2);
-    K = table.K_20((table.BHN_steel == 150 & string(table.other_material) == "Bronze"), :);
 end
 
 end
