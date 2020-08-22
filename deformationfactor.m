bd6dcab1516cd1883bcf2677a1f220fb785b0115
seknaingphyo <snpnineeleven@gmail.com>
2020-08-22 08:03:51 +0630

    Update deformationfactor.m

--- deformationfactor.m
+++ deformationfactor.m
@@ -1,7 +1,9 @@
 function C = deformationfactor(error, material1, material2, profile)
 
 filename = 'D:\Matlab R202a\bin\Matlab Projects\Gear Designer\Spur Gear Designer\Data Files\Deformationfactor.xlsx';
-table = readtable(filename);
+d = detectImportOptions(filename);
+d.DataRange = 'A1';
+table = readtable(filename, d);
 
 numericHeaders = table(2,:);
 numericHeaders = rmmissing(numericHeaders, 2);
@@ -11,7 +13,8 @@ table.Properties.VariableNames(end - numel(numericHeaders) + 1 : end) = numericH
 table.Properties.VariableNames(1:end - numel(numericHeaders)) = table2array(rmmissing(table(1,:),2));
 table(1:2, :) = [ ];
 
-idx = table(table.Material1 == material1 & table.Material2 == material2 & table.Toothform == profile);
-C = interp1(idx(:, 4:end), error);
+idx = ismember(table.Material1, material1) & ismember(table.Material2, material2) & ismember(table.Toothform, profile);
+tootherror = str2double(numericHeaders);
+C = interp1(tootherror, table2array(table(idx, 4:end)), error);
 
 end
