d8775d92e16d4046cf910b9837e5058f0673c1c0
seknaingphyo <snpnineeleven@gmail.com>
2020-08-22 13:23:22 +0630

    Update StressFatigueFactor.m

--- StressFatigueFactor.m
+++ StressFatigueFactor.m
@@ -1,5 +1,6 @@
-function K = StressFatigueFactor(material1, material2, BHN, profile)
+function K = StressFatigueFactor(material1, material2, BHN1, BHN2, profile)
 
+BHN_avg = (BHN1+BHN2)/2;
 Steel1 = strncmp(material1, 'Steel', 5);
 Steel2 = strncmp(material2, 'Steel', 5);
 Cast_Iron1 = strncmp(material1, 'Cast Iron', 9);
@@ -9,29 +10,24 @@ profile20 = strncmp(profile, 'Full Depth 20', 13) | strncmp(profile, 'Stub 20', 
 
 filename = 'D:\Matlab R202a\bin\Matlab Projects\Gear Designer\Spur Gear Designer\Data Files\Stress Fatigue Factor.xlsx';
 
-
 if Steel1 == 1 && Steel2 == 1 && profile14 == 1
     table = readtable(filename, 'Sheet', 1);
-    idx = table.Average_BHN_steel(BHN);
-    K = table.K_14(idx);
+    interp1(table.Average_BHN_steel, table.K_14, BHN_avg);
 elseif Steel1 == 1 && Steel2 == 1 && profile20 == 1
     table = readtable(filename, 'Sheet', 1);
-    idx = table.Average_BHN_steel(BHN);
-    K = table.K_20(idx);
+    interp1(table.Average_BHN_steel, table.K_20, BHN_avg);
 elseif Cast_Iron1 == 1 && Cast_Iron2 == 1 && profile14 == 1
     table = readtable(filename, 'Sheet', 3);
-    idx = table.Material(material1) | table.Material(material2);
-    K = table.K_14(idx);
+    K = table.K_14;
 elseif Cast_Iron1 == 1 && Cast_Iron2 == 1 && profile20 == 1
     table = readtable(filename, 'Sheet', 3);
-    idx = table.Material(material1) | table.Material(material2);
-    K = table.K_20(idx);
+    K = table.K_20;
 elseif Steel1 == 1 && Cast_Iron2 == 1 && profile14 == 1
     table = readtable(filename, 'Sheet', 2);
-    K = table.K_14((table.BHN_steel == 150 & string(table.other_material) == "Cast Iron"), :);
+    K = interp1(table.BHN_steel, table.K_14, BHN_avg);
 elseif Steel1 == 1 && Cast_Iron2 == 1 && profile20 == 1
     table = readtable(filename, 'Sheet', 2);
-    K = table.K_20((table.BHN_steel == 150 & string(table.other_material) == "Cast Iron"), :);
+    K = interp1(table.BHN_steel, table.K_20, BHN_avg);
 end
 
 end
