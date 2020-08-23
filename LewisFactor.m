7e36d474c09809b92149cf6e9955ed596dda58e7
seknaingphyo <snpnineeleven@gmail.com>
2020-08-23 12:09:51 +0630

    Update LewisFactor.m

--- LewisFactor.m
+++ LewisFactor.m
@@ -3,13 +3,13 @@ function y = LewisFactor(numofteeth, profile)
 filename = 'D:\Matlab R202a\bin\Matlab Projects\Gear Designer\Spur Gear Designer\Data Files\Lewis_factor_table.xlsx';
 table = readtable(filename);
 
-if strncmp('Involute 14.5', profile, 13) == 1
+if strcmp('Involute 14.5', profile) == 1
     table = removevars(table, {'Full_Depth_20', 'Stub_20'});
     y = interp1(table.Numofteeth, table.Full_Depth_14, numofteeth);
-elseif strncmp('Full Depth 20', profile, 13) == 1
+elseif strcmp('Full Depth 20', profile) == 1
     table = removevars(table, {'Full_Depth_14', 'Stub_20'});
     y = interp1(table.Numofteeth, table.Full_Depth_20, numofteeth);
-elseif strncmp('Stub 20', profile, 7) == 1
+elseif strcmp('Stub 20', profile) == 1
     table = removevars(table, {'Full_Depth_14', 'Full_Depth_20'});
     y = interp1(table.Numofteeth, table.Stub_20, numofteeth);
 end
