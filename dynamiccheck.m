2efabf10f5dd0f1d91ca1357cc3261ff9e576ca9
seknaingphyo <snpnineeleven@gmail.com>
2020-09-03 20:58:48 +0630

    Update dynamiccheck.m

--- dynamiccheck.m
+++ dynamiccheck.m
@@ -1,12 +1,10 @@
-function [true_module, dia, face_width, tot_error, F_end, F_dynamic, F_wear] = dynamiccheck(face_width, true_module, dia, K, Q, strength, y, M_t, rpm, material1, material2, profile)
-
-speed = (pi*dia*10^-3*rpm)/60;
+function [true_module, dia, face_width, tot_error, F_end, F_dynamic, F_wear] = dynamiccheck(face_width, true_module, dia, K, Q, strength, y, M_t, rpm, material1, material2, profile, N, speed)
 
 error_avg = tootherror(true_module, speed);
 
 C = deformationfactor(error_avg, material1, material2, profile);
 
-F_end = strength*10^6*face_width*10^-3*y*pi*true_module*10^-3;
+F_end = strength*face_width*y*pi*true_module;
 
 F_t = M_t/((dia*10^-3)/2);
 
@@ -29,11 +27,11 @@ while F_dynamic > F_wear && F_dynamic > F_end && error1 >= 0.11 && error2 >= 0.1
     speed = (pi*dia*10^-3*rpm)/60;
     error_avg = tootherror(true_module, speed);
     C = deformationfactor(error_avg, material1, material2, profile);
-    F_dynamic = F_t + ((21*speed*(face_width*C) + F_t)/(21*V + sqrt(face_width*C + F_t)));
+    F_dynamic = F_t + ((21*speed*(face_width*C) + F_t)/(21*speed + sqrt(face_width*C + F_t)));
     
     if F_dynamic < F_wear && F_dynamic < F_end && error1 < 0.11 && error2 < 0.11
         continue
-    elseif F_dynamic > F_wear && F_dynamic > F_end && error1 < 0.11 && error2 < 0.11
+    elseif ((F_dynamic > F_wear && F_dynamic < F_end) || (F_dynamic < F_wear && F_dynamic > F_end)) && error1 < 0.11 && error2 < 0.11
         face_width = round(4*pi*true_module);
         continue
     end
