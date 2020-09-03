a748527b9ad8722e7692cd99bf1738751bf76477
seknaingphyo <snpnineeleven@gmail.com>
2020-09-03 19:07:58 +0630

    Update dynamiccheck.m

--- dynamiccheck.m
+++ dynamiccheck.m
@@ -1,4 +1,4 @@
-function [true_module, dia, face_width] = dynamiccheck(face_width, true_module, dia, speed, C, K, Q, strength, y, M_t)
+function [true_module, dia, face_width, error_avg] = dynamiccheck(face_width, true_module, dia, speed, C, K, Q, strength, y, M_t)
 
 F_end = strength*10^6*face_width*y*pi*true_module*10^-3;
 
@@ -12,6 +12,8 @@ error1 = abs((F_dynamic - F_end)/F_dynamic);
 
 error2 = abs((F_dynamic - F_wear)/F_dynamic);
 
+error_avg = ((error1 + error2)/2)*100;
+
 while F_dynamic > F_wear && F_dynamic > F_end && error1 >= 0.11 && error2 >= 0.11
     
     true_module = true_module + 1;    
@@ -19,8 +21,8 @@ while F_dynamic > F_wear && F_dynamic > F_end && error1 >= 0.11 && error2 >= 0.1
     F_wear = dia*face_width*K*Q;
     F_t = M_t/(dia/2);
     speed = (pi*dia*10^-3*rpm)/60;
-    error = tootherror(true_module, speed);
-    C = deformationfactor(error, material1, material2, profile);
+    error_avg = tootherror(true_module, speed);
+    C = deformationfactor(error_avg, material1, material2, profile);
     F_dynamic = F_t + ((21*speed*(face_width*C) + F_t)/(21*V + sqrt(face_width*C + F_t)));
     
     if F_dynamic < F_wear && F_dynamic < F_end && error1 < 0.11 && error2 < 0.11
