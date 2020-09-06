990d4f5a046c0e4b0db1b00f0907e89942deb893
seknaingphyo <snpnineeleven@gmail.com>
2020-09-06 20:55:34 +0630

    Update ModuleCorrector3.m

--- ModuleCorrector3.m
+++ ModuleCorrector3.m
@@ -1,4 +1,4 @@
-function[N, true_module, ind_s, allowed_s] = ModuleCorrector3(dia, assum_module, y_assum, profile)
+function[N, true_module, allowed_s, ind_s] = ModuleCorrector3(dia, assum_module, y_assum, profile)
 
 allowed_s = 1/(((assum_module*10^-3)^2)*y_assum);
 N = dia/assum_module;
@@ -6,6 +6,10 @@ y = LewisFactor(N, profile);
 ind_s = 1/(((assum_module*10^-3)^2)*y);
 diff = ind_s - allowed_s;
 percent = (diff/allowed_s)*100;
+
+if allowed_s > ind_s && diff < 0 && percent <= -25
+        true_module = assum_module;
+end
 
 while ind_s > allowed_s && diff > 0 && percent > 0
     
@@ -22,8 +26,16 @@ while ind_s > allowed_s && diff > 0 && percent > 0
     
     y = LewisFactor(N, profile);
     ind_s = 1/(((assum_module*10^-3)^2)*y);
-    
-    if allowed_s > ind_s && diff < 0 && percent > -25
+    diff = ind_s - allowed_s;
+    percent = (diff/allowed_s)*100;
+           
+    if allowed_s > ind_s && diff < 0 && percent >= -25
+        true_module = assum_module;
+        continue
+    end
+end
+ 
+while allowed_s > ind_s && diff < 0 && percent < -25
         assum_module = assum_module - 1;
         allowed_s = 1/(((assum_module*10^-3)^2)*y_assum);
         N = dia/assum_module;
@@ -33,11 +45,14 @@ while ind_s > allowed_s && diff > 0 && percent > 0
             N = dia/assum_module;
         elseif rem(N, 1) == 0
             continue
-        end    
+        end
+        
         y = LewisFactor(N, profile);
         ind_s = 1/(((assum_module*10^-3)^2)*y);
+        diff = ind_s - allowed_s;
+        percent = (diff/allowed_s)*100;
         
-    elseif allowed_s > ind_s && diff < 0 && percent <= -25
+    if allowed_s > ind_s && diff < 0 && percent >= -25
         true_module = assum_module;
         continue
     end
