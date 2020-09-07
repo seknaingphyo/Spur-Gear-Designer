e28250d2d3d9482c0d3be3e8f72bf63b83d9ef4d
seknaingphyo <snpnineeleven@gmail.com>
2020-09-07 19:58:13 +0630

    Update dynamiccheck.m

--- dynamiccheck.m
+++ dynamiccheck.m
@@ -16,7 +16,7 @@ error1 = abs((F_dynamic - F_end)/F_dynamic);
 
 error2 = abs((F_dynamic - F_wear)/F_dynamic);
 
-while F_dynamic > F_wear && F_dynamic > F_end && error1 >= 0.11 && error2 >= 0.11
+while (F_dynamic > F_wear && F_dynamic > F_end) && (error1 >= 0.11 || error2 >= 0.11)
     
     true_module = true_module + 1;    
     dia = N*true_module;
@@ -27,7 +27,7 @@ while F_dynamic > F_wear && F_dynamic > F_end && error1 >= 0.11 && error2 >= 0.1
     C = deformationfactor(error_avg, material1, material2, profile);
     F_dynamic = F_t + ((21*speed*(face_width*C) + F_t)/(21*speed + sqrt(face_width*C + F_t)));
     
-    if F_dynamic < F_wear && F_dynamic < F_end && error1 < 0.11 && error2 < 0.11
+    if (F_dynamic < F_wear && F_dynamic < F_end) && (error1 < 0.11 && error2 < 0.11)
         continue
     elseif ((F_dynamic > F_wear && F_dynamic < F_end) || (F_dynamic < F_wear && F_dynamic > F_end)) && error1 < 0.11 && error2 < 0.11
         face_width = round(4*pi*true_module);
