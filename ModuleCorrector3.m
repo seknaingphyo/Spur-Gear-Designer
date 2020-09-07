038a5a6812b6cf3cb786a140175d64dec148144a
seknaingphyo <snpnineeleven@gmail.com>
2020-09-07 11:57:06 +0630

    Update ModuleCorrector3.m

--- ModuleCorrector3.m
+++ ModuleCorrector3.m
@@ -4,14 +4,14 @@ allowed_s = 1/(((assum_module*10^-3)^2)*y_assum);
 N = dia/assum_module;
 y = LewisFactor(N, profile);
 ind_s = 1/(((assum_module*10^-3)^2)*y);
-diff = ind_s - allowed_s;
+diff = allowed_s - ind_s;
 percent = (diff/allowed_s)*100;
 
-if allowed_s > ind_s && percent >= -25
+if allowed_s > ind_s && percent <= 25
         true_module = assum_module;
 end
 
-while ind_s > allowed_s && percent > 0    
+while ind_s > allowed_s && percent < 0    
     assum_module = assum_module + 1;
     N = dia/assum_module;
     
@@ -29,13 +29,13 @@ while ind_s > allowed_s && percent > 0
     diff = ind_s - allowed_s;
     percent = (diff/allowed_s)*100;
            
-    if allowed_s > ind_s && diff < 0 && percent >= -25
+    if allowed_s > ind_s && diff < 0 && percent <= 25
         true_module = assum_module;
         continue
     end
 end
  
-while allowed_s > ind_s && percent < -25
+while allowed_s > ind_s && percent > 25
         assum_module = assum_module - 1;
         N = dia/assum_module;
     
@@ -53,7 +53,7 @@ while allowed_s > ind_s && percent < -25
         diff = ind_s - allowed_s;
         percent = (diff/allowed_s)*100;
         
-    if allowed_s > ind_s && percent >= -25
+    if allowed_s > ind_s && percent <= 25
         true_module = assum_module;
         continue
     end
