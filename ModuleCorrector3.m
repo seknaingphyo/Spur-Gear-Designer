cb46b9c0bacfe3252b25bf50120b86d6ffb40d7c
seknaingphyo <snpnineeleven@gmail.com>
2020-09-07 11:37:03 +0630

    Update ModuleCorrector3.m

--- ModuleCorrector3.m
+++ ModuleCorrector3.m
@@ -7,23 +7,23 @@ ind_s = 1/(((assum_module*10^-3)^2)*y);
 diff = ind_s - allowed_s;
 percent = (diff/allowed_s)*100;
 
-if allowed_s > ind_s && diff < 0 && percent <= -25
+if allowed_s > ind_s && percent >= -25
         true_module = assum_module;
 end
 
-while ind_s > allowed_s && diff > 0 && percent > 0
-    
+while ind_s > allowed_s && percent > 0    
     assum_module = assum_module + 1;
-    allowed_s = 1/(((assum_module*10^-3)^2)*y_assum);
     N = dia/assum_module;
     
-    if rem(N, 1) ~= 0
+    while rem(N, 1) ~= 0
         assum_module = assum_module + 1;
         N = dia/assum_module;
-    elseif rem(N, 1) == 0
-        continue
+        if rem(N, 1) == 0
+            continue
+        end
     end
     
+    allowed_s = 1/(((assum_module*10^-3)^2)*y_assum);
     y = LewisFactor(N, profile);
     ind_s = 1/(((assum_module*10^-3)^2)*y);
     diff = ind_s - allowed_s;
@@ -35,26 +35,28 @@ while ind_s > allowed_s && diff > 0 && percent > 0
     end
 end
  
-while allowed_s > ind_s && diff < 0 && percent < -25
+while allowed_s > ind_s && percent < -25
         assum_module = assum_module - 1;
-        allowed_s = 1/(((assum_module*10^-3)^2)*y_assum);
         N = dia/assum_module;
     
-        if rem(N, 1) ~= 0
+        while rem(N, 1) ~= 0
             assum_module = assum_module - 1;
             N = dia/assum_module;
-        elseif rem(N, 1) == 0
-            continue
+            if rem(N, 1) == 0
+                continue
+            end
         end
         
+        allowed_s = 1/(((assum_module*10^-3)^2)*y_assum);        
         y = LewisFactor(N, profile);
         ind_s = 1/(((assum_module*10^-3)^2)*y);
         diff = ind_s - allowed_s;
         percent = (diff/allowed_s)*100;
         
-    if allowed_s > ind_s && diff < 0 && percent >= -25
+    if allowed_s > ind_s && percent >= -25
         true_module = assum_module;
         continue
     end
 end
+
 end
